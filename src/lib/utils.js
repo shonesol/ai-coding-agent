731f863bfdfa8db4c78990547f005ee9701ac589
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-09-07 18:15:58 +0300

    Add files via upload

---
+++ src/lib/utils.js
@@ -0,0 +1,15 @@
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(date)
+}
